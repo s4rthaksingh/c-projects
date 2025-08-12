943da17e82535ed1263fabb299792e9433ad86f5
Sarthak Singh <sarthakss898@gmail.com>
2025-08-12 13:38:08 +0000

    List of strings 2.0

--- pointers/stringlist/sl.c
+++ pointers/stringlist/sl.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void printarray(char **arr, int size){
 	for(int j = 0; j < size; j++){
 		printf("%s",arr[j]);
 
-}
+	}
+	printf("\n");
 }
 
 int main(){
 	char **arr = malloc(sizeof(char *));
-	char *str;
-	size_t temp;
+	char *str = "Hello World";
+	size_t temp = 0;
 	int i = 0;	
+	ssize_t olen = 0;	
+	ssize_t nlen = 0;
 
-	while(1){
+	while(*str != '-'){
+		olen = nlen;
 		arr = realloc(arr, (i + 1)*sizeof(char *)); 
 		printf("Enter string : ");
-		getline(&str,&temp,stdin);
-		arr[i] = str;
+		nlen = getline(&str,&temp,stdin);
+		str[nlen-1] = ',';
+		arr[i] = strdup(str);
 
-		printarray(arr, i + 1);	
 		i++;
-		str = NULL;
-		temp = 0;
 	}
-
+	
+	arr[i-2][olen-1] = '\0';
+	arr[i-1] = "";
+	printarray(arr, i);
+	
 	return 0;
 }
