8018560fc1f285ecf7e8524f4ae9d488c2ba9194
Sarthak Singh <sarthakss898@gmail.com>
2025-08-17 04:37:39 +0000

    Error handling for copy program

--- shell/copy.c
+++ shell/copy.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+	if(argc != 3) {
+		printf("Usage: ./copy <source.txt> <target.txt>\n");
+		return 1;
+	}
+
 	char *src = argv[1];	
 	char *dest = argv[2];
 
 	FILE *srcfile = fopen(src, "r");
+	if(!srcfile){
+		perror("Error opening source file");
+		return 1;
+	}
 	FILE *destfile = fopen(dest, "w");
-	char ch;
+	if(!destfile){
+		perror("Error opening destination file");
+		return 1;
+	}
+	int ch;
 	while((ch = fgetc(srcfile)) != EOF)
 		fputc(ch, destfile);
 	printf("Successfully copied %s to %s\n", src, dest);
+	fclose(srcfile);
+	fclose(destfile);
 	return 0;
 }
