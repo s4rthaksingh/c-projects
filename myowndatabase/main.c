80e5d149bcb60bb4cf7c9716261fa2749fd2b27a
Sarthak Singh <sarthakss898@gmail.com>
2025-09-13 12:25:12 +0000

    Some rearrangement, and new database function : check data length

---
+++ myowndatabase/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	size_t len = 0;
+	char *line = NULL;
+	printf("> ");
+	ssize_t read = getline(&line, &len, stdin);
+	printf("Data length : %ld\n", read-1);
+	return 0;
+};
