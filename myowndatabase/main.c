b77555303bc81f46d216d915a2be94a2e999ad07
Sarthak Singh <sarthakss898@gmail.com>
2025-09-15 16:35:56 +0000

    Write data to a file

--- myowndatabase/main.c
+++ myowndatabase/main.c
@@ -2,9 +2,12 @@
 
 int main(){
 	size_t len = 0;
+	FILE *fptr = fopen("data.txt", "w");
 	char *line = NULL;
 	printf("> ");
 	ssize_t read = getline(&line, &len, stdin);
+	fprintf(fptr,"%s", line);
+	fclose(fptr);
 	printf("Data length : %ld\n", read-1);
 	printf("Data size : %ld\n", sizeof(line));
 	return 0;
