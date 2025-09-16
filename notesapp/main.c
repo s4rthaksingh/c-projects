dcb7c42c0a073cba033a4be716f47d32d5912f1b
Sarthak Singh <sarthakss898@gmail.com>
2025-09-16 06:34:55 +0000

    Take filename input from user

--- notesapp/main.c
+++ notesapp/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	size_t len = 0;
-	FILE *fptr = fopen("data.txt", "a");
 	char c;
-	printf("Enter a new note, and press CTRL+D to end : ");
+	char userfilename[20];
+	char filename[20] = "data.txt";
+	printf("Enter the filename you want to store text in (leave blank for 'data.txt') : ");
+	scanf("%s", userfilename);
+	if(strlen(userfilename) > 3)
+		strcpy(filename, userfilename);
+	FILE *fptr = fopen(filename, "a");
+	printf("\nEnter a new note, and press CTRL+D to end : ");
 	fprintf(fptr, "- ");
 	while((c = getchar()) != EOF){
 		fputc(c,fptr);
 	}
-	printf("\nSuccessfully written data to file : data.txt\n");
+	printf("\nSuccessfully written data to file : %s\n", filename);
 	fclose(fptr);
 	return 0;
 };
