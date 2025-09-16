4715b7c922b72ae3c17b9e6484b8cc5d8ad26e50
Sarthak Singh <sarthakss898@gmail.com>
2025-09-16 06:25:40 +0000

    New pointers for each note, now notes file doesnt overwrites but appends

--- notesapp/main.c
+++ notesapp/main.c
@@ -2,9 +2,10 @@
 
 int main(){
 	size_t len = 0;
-	FILE *fptr = fopen("data.txt", "w");
+	FILE *fptr = fopen("data.txt", "a");
 	char c;
 	printf("Enter a new note, and press CTRL+D to end : ");
+	fprintf(fptr, "- ");
 	while((c = getchar()) != EOF){
 		fputc(c,fptr);
 	}
