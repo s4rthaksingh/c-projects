97a83beb07d30c3f9e2ec264465b8f1afe980c83
Sarthak Singh <sarthakss898@gmail.com>
2025-09-16 06:20:48 +0000

    Write data to text file, and allow newlines

--- myowndatabase/main.c
+++ myowndatabase/main.c
@@ -2,11 +2,13 @@
 
 int main(){
 	size_t len = 0;
-	FILE *fptr = fopen("data.bin", "wb");
-	char *line = NULL;
-	printf("> ");
-	ssize_t read = getline(&line, &len, stdin);
-	fprintf(fptr,"%s", line);
+	FILE *fptr = fopen("data.txt", "w");
+	char c;
+	printf("Enter a new note, and press CTRL+D to end : ");
+	while((c = getchar()) != EOF){
+		fputc(c,fptr);
+	}
+	printf("\nSuccessfully written data to file : data.txt\n");
 	fclose(fptr);
 	return 0;
 };
